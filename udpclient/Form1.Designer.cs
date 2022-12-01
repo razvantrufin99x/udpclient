8894763755908a115a48b4f0527eb88e2e4e5b38
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 21:29:36 +0200

    Add project files.

---
+++ udpclient/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace udpclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(24, 12);
+            this.tbHost.Multiline = true;
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(285, 79);
+            this.tbHost.TabIndex = 1;
+            this.tbHost.Text = "192.168.1.2:80";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 161);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "udp client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbHost;
+    }
+}
+
---
+++ udpclient/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace udpclient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(tbHost.Text, 8080);
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("Hello World?");
+            udpClient.Send(sendBytes, sendBytes.Length);
+
+        }
+    }
+}
